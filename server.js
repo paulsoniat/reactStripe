ae1e744bacd934c2d063fd5694638cad5bf3fe53
Paul Soniat <paulmsoniat@gmail.com>
2018-05-21 16:15:27 -0500

    [server] adds create user route

--- server.js
+++ server.js
@@ -4,12 +4,36 @@ const fs = require('fs');
 const axios = require('axios');
 const db = require('./client/database/db.js');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
 const port = process.env.PORT || 5000;
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+let opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromHeader('authorization'),
+  opts.secretOrKey = 'secret';
+passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+  Users.findOne({
+    username: jwt_payload.username
+  }, (err, user) => {
+    if (err) {
+      return done(err, false);
+    } else if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
+}));
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
@@ -19,41 +43,54 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.post('/handleNewUser', function (req, res) {
-  console.log(req.body, "this is body")
-  if (!req.body.email || !req.body.password) {
-    res.send("must enter a valid email and password")
-  } else {
-    var statusCode = 201
-    const newUser = new db.User({
-      email: req.body.email,
-      password: req.body.password
+app.post('/createUser', function (req, res) {
+  const tokenData = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  db.User.findOne({
+      email: req.body.email
     })
-    newUser.save((err) => {
-      if (err) {
-        return console.error(err)
+    .then((results) => {
+      if (results === null) {
+        const user = new db.User(tokenData);
+        user.save((err, createdUser) => {
+          if (err) {
+            console.error(err);
+            res.status(400).send('there was an error creating the user');
+          } else {
+            tokenData._id = createdUser._id;
+            const token = jwt.sign(tokenData, 'secret');
+            res.status(201).send(token);
+          }
+        })
+      } else {
+        res.status(401).send('Sorry, a user with that name already exists');
       }
-    });
-    res.send(statusCode, req.body)
-  }
+    })
 })
 
-app.post('/handleExistingUser', function (req, res) {
-  console.log(req.body, "this is body")
-  if (!req.body) {
-    return res.sendStatus(400)
-  } else {
-    var statusCode = 201
-    const newUser = new db.User({
-      username: req.body.username
+app.post('/loginUser', function (req, res) {
+  db.User.findOne({
+      email: req.body.email
     })
-    newUser.save((err) => {
-      if (err) {
-        return console.error(err)
+    .then((user) => {
+      if (user.password !== req.body.password) {
+        res.send('Sorry, that password was incorrect');
+      } else {
+        const tokenData = {
+          id: user._id,
+          username: user.username,
+        };
+        const token = jwt.sign(tokenData, 'secret');
+        res.status(201).send(token);
       }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(404).send(err);
     });
-    res.send(statusCode, req.body)
-  }
+
 })
 
 
